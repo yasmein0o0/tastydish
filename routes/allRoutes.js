437e0ada92c5d3955f6dd2355a9b14d8b674d8c0
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-07-26 16:23:00 +0300

    add: receiep details route

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { home } from '../controllers/home.js';
+import { receips } from '../controllers/receipDetails.js';
 const router = express.Router();
 
 router.get('/', home)
 router.get('/home', home)
+router.get('/:id', receips)
 
 export default router
