15ff9277c9a04bb6f4fa872fee63a32d05b90d25
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-07-27 01:59:18 +0300

    add: search results route

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { home } from '../controllers/home.js';
 import { receips } from '../controllers/receipDetails.js';
 import { autoComplete } from '../controllers/autoComplete.js';
+import { searchResults } from '../controllers/searchResults.js';
 const router = express.Router();
 
 router.get('/', home)
 router.get('/home', home)
 router.get('/:id', receips)
 router.post('/autocomplete', autoComplete)
+router.post('/search', searchResults)
 
 export default router
