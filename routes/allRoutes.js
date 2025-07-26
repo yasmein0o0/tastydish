5805470cfb5fddbb412e80c3f56e2b0009a9c8b2
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-07-26 23:39:15 +0300

    add: autoComplete route

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { home } from '../controllers/home.js';
 import { receips } from '../controllers/receipDetails.js';
+import { autoComplete } from '../controllers/autoComplete.js';
 const router = express.Router();
 
 router.get('/', home)
 router.get('/home', home)
 router.get('/:id', receips)
+router.post('/autocomplete', autoComplete)
 
 export default router
