a7e71b624f8c94c6d7f91218055d2e9578e3c0d3
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-07-31 14:52:31 +0300

    add: sign up controller

---
+++ controllers/signup.js
@@ -0,0 +1,63 @@
+import { pool } from "../database/pg.js";
+import xss from "xss";
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+    console.log('sent')
+    const username = xss(req.body.name);
+    const email = xss(req.body.email);
+    const password = xss(req.body.password);
+
+    //check inputs
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: "All fields are required." });
+    }
+
+    try {
+        //check if email is already signed up
+        const checkEmail = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        if (checkEmail.rows.length > 0) {
+            return res.status(409).json({ message: `${email} is already used` });
+        }
+
+        //1- hash password
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        //2- generate refresh token
+        const refreshToken = jwt.sign({ email }, process.env.JWT_REFRESH_SECRET, { expiresIn: "30d" })
+
+        //3- generate access token
+        const accessToken = jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, { expiresIn: "30m" })
+
+        //4- save username, email, password, refersh token in db
+        const result = await pool.query('INSERT INTO users(name,email,password,refresh_token) values($1,$2,$3,$4) RETURNING *',
+            [username, email, hashedPassword, refreshToken]);
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 15 * 60 * 1000,
+        });
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+        });
+
+        const { id, name, email: savedEmail } = result.rows[0];
+        console.log(accessToken)
+        return res.status(201).json({
+            message: "Signup successful",
+            user: { id, name, email: savedEmail }
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: `server error: ${error}` })
+    }
+
+}
