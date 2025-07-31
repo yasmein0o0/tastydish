3a4d0b1c1bd5959db9769ef7718e78597c766cd5
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-07-31 14:59:44 +0300

    add: signup route

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import { validationResult } from 'express-validator';
 import { home } from '../controllers/home.js';
 import { receips } from '../controllers/receipDetails.js';
 import { autoComplete } from '../controllers/autoComplete.js';
 import { searchResults } from '../controllers/searchResults.js';
+import contactValidation from '../middlewares/contactUsValidation.js';
+import { contactUs } from '../controllers/contactUs.js';
+import { signupValidation } from '../middlewares/signupValidation.js';
+import { resultOfValidation } from '../middlewares/resultOfValidation.js';
+import { signup } from '../controllers/signup.js';
+import { blockIfLogin } from '../middlewares/blockIfLogged.js';
 const router = express.Router();
 
 router.get('/', home)
@@ -11,4 +18,9 @@ router.get('/:id', receips)
 router.post('/autocomplete', autoComplete)
 router.post('/search', searchResults)
 
+router.post('/contact', contactValidation, resultOfValidation, contactUs);
+
+
+//auth
+router.post('/signup', signupValidation, resultOfValidation, blockIfLogin, signup)
 export default router
