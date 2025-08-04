7526cd3ea4b05c5e4671c30924c8f87f6cf17cde
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-08-04 15:47:07 +0300

    add favourite routes and middlewares for sign up, log in and other auth routes

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { validationResult } from 'express-validator';
 import { home } from '../controllers/home.js';
 import { receips } from '../controllers/receipDetails.js';
 import { autoComplete } from '../controllers/autoComplete.js';
@@ -10,6 +9,14 @@ import { signupValidation } from '../middlewares/signupValidation.js';
 import { resultOfValidation } from '../middlewares/resultOfValidation.js';
 import { signup } from '../controllers/signup.js';
 import { blockIfLogin } from '../middlewares/blockIfLogged.js';
+import { verifyToken } from '../middlewares/verifyToken.js';
+import { logOut } from '../controllers/logout.js';
+import { loginValidation } from '../middlewares/loginValidation.js';
+import { login } from '../controllers/login.js';
+import { refreshToken } from '../controllers/refresh.js';
+import { deleteAccount } from "../controllers/deleteAccount.js";
+import { logInRateLimit, refreshLimiter } from '../middlewares/rateLimit.js';
+import { addToFavourites, removeFromFavourites, getFavourites } from '../controllers/favourites.js';
 const router = express.Router();
 
 router.get('/', home)
@@ -18,9 +25,19 @@ router.get('/:id', receips)
 router.post('/autocomplete', autoComplete)
 router.post('/search', searchResults)
 
-router.post('/contact', contactValidation, resultOfValidation, contactUs);
+router.post('/contact', logInRateLimit, contactValidation, resultOfValidation, contactUs);
 
 
 //auth
-router.post('/signup', signupValidation, resultOfValidation, blockIfLogin, signup)
+router.post('/signup', logInRateLimit, blockIfLogin, signupValidation, resultOfValidation, signup);
+router.post('/log-out', verifyToken, logOut)
+router.post('/log-in', logInRateLimit, blockIfLogin, loginValidation, resultOfValidation, login)
+router.post('/refresh', refreshLimiter, refreshToken)
+router.delete("/delete-account", verifyToken, deleteAccount);
+
+// Add to Favourites 
+router.post("/add-to-favourites", verifyToken, addToFavourites);
+router.post("/delete-from-favourites", verifyToken, removeFromFavourites);
+router.post("/get-favourites", verifyToken, getFavourites);
+
 export default router
