dd5f7b3746e0dafac7acb7699efc646abcd742a2
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-08-09 18:55:32 +0300

    fix: status code to 403 to treat the resquest as fforbidden

--- middlewares/blockIfLogged.js
+++ middlewares/blockIfLogged.js
@@ -7,6 +7,6 @@ export const blockIfLogin = (req, res, next) => {
     if (!token) return next();
     jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err) => {
         if (err) return next();
-        return res.status(400).json({ message: 'you are already logged in' });
+        return res.status(403).json({ message: 'you are already logged in' });
     })
 }
