a4244e7f3d99bee3cc01360e315832b8e256f093
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-10-01 22:24:26 +0300

    edit: make middleware async, get user info from database to return to the client, fix: signing accesstoken by only ading user id and remove email and name

--- controllers/refresh.js
+++ controllers/refresh.js
@@ -1,22 +1,29 @@
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import { pool } from '../database/pg.js';
 
-export const refreshToken = (req, res) => {
+export const refreshToken = async (req, res) => {
     const token = req.cookies.refreshToken;
     if (!token) return res.status(401).json({ message: "No refresh token provided" });
 
-    jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_REFRESH_SECRET, async (err, user) => {
         if (err) return res.status(403).json({ message: "Invalid refresh token" });
 
+        const userInfo = await pool.query('SELECT * FROM users WHERE id = $1', [user.id])
+        const { id, email, name } = userInfo.rows[0]
+
+
         const accessToken = jwt.sign(
-            { id: user.id, email: user.email },
+            { id: userInfo.rows[0].id },
             process.env.JWT_ACCESS_SECRET,
             { expiresIn: '30m' }
         );
 
+
         res.status(200).json({
             message: "Access token refreshed",
-            accessToken
+            accessToken,
+            user: { id, name, email },
         });
     });
 };
