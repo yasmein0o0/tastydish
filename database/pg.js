818c809ba2990f12f146ebe44ff611bf6dce54d1
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-10-02 03:42:43 +0300

    edit: database conection url instead of separate values

--- database/pg.js
+++ database/pg.js
@@ -1,10 +1,9 @@
 import "dotenv/config"
 import { Pool } from "pg";
 
-export const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
-    password: process.env.PASSWORD
-})
+const pool = new Pool({
+    connectionString: process.env.DATABASE,
+    ssl: { rejectUnauthorized: false }
+});
+
+export default pool;
